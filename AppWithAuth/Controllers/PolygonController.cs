cc7f9725628cd5ea410a4d4c39376f81574d133e
DimaRubio <testdlyasites@gmail.com>
2017-09-20 18:59:11 +0300

    learned Razor, Layout

---
+++ AppWithAuth/Controllers/PolygonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWithAuth.Controllers
+{
+    public class PolygonController : Controller
+    {
+        // GET: Polygon
+        public ActionResult Polygon()
+        {
+            return View();
+        }
+    }
+}

9adf4b5d7edadec33f0e6355630b75fa48d1973c
DimaRubio <testdlyasites@gmail.com>
2017-10-25 17:00:26 +0300

    webconfig controller

---
+++ AppWithAuth/Controllers/WebConfigController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Configuration;
+
+namespace AppWithAuth.Controllers
+{
+    public class WebConfigController : Controller
+    {
+        // GET: WebConfig
+        public ActionResult Index()
+        {
+            //this.Header.Title = WebConfigurationManager.AppSettings["websiteName"];
+            //txtHeader.Text = Header.Title;
+            //txt1.Text = WebConfigurationManager.AppSettings["welcomeMessage"];
+            ViewBag.ClientValidation = WebConfigurationManager.AppSettings["ClientValidationEnabled"];
+            return View();
+        }
+    }
+}
